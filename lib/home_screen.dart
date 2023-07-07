9fd01ae7263b0f1555bff2e42fa7c484570dc59d
MarekRudzki <102899533+MarekRudzki@users.noreply.github.com>
2023-07-07 14:59:15 +0200

    Init commit

---
+++ lib/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('City'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:pogodappka/home_screen.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      title: 'Pogodappka',
+      home: HomeScreen(),
+    ),
+  );
+}
