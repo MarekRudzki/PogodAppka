e5a66862964f6fbd801f10e6cb5865bd1e9dc6de
MarekRudzki <102899533+MarekRudzki@users.noreply.github.com>
2023-07-15 12:05:12 +0200

    Secure API Key

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:pogodappka/screens/home_screen/home_screen.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
   runApp(
     const MaterialApp(
       title: 'Pogodappka',
